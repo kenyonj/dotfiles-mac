30b936594464269c739d9e8955b2df5118401690
Justin Kenyon <kenyonj@gmail.com>
2018-09-19 13:29:35 -0400

    Update to porcelain

--- tmux-status-line.sh
+++ tmux-status-line.sh
@@ -4,14 +4,14 @@ CLEAN_BRANCH_COLOR="#[fg=colour16, bg=colour2]"
 DIRTY_BRANCH_COLOR="#[fg=colour16, bg=colour1]"
 STAGED_BRANCH_COLOR="#[fg=colour16, bg=colour3]"
 
-branch_color() {
-  local git_status=$(cd $1; git status -unormal 2>&1)
+git_status_color() {
+  local git_status=$(cd $1; git status --porcelain 2>&1)
 
-  if [[ "$git_status" =~ Changes\ not\ staged ]]; then
+  if [[ "$git_status" =~ MM ]]; then
     echo -n $DIRTY_BRANCH_COLOR
-  elif [[ "$git_status" =~ Your\ branch\ is\ ahead\ of ]]; then
+  elif [[ "$git_status" =~ M ]]; then
     echo -n $STAGED_BRANCH_COLOR
-  elif [[ "$git_status" =~ nothing\ to\ commit ]]; then
+  else
     echo -n $CLEAN_BRANCH_COLOR
   fi
 }
@@ -19,7 +19,7 @@ branch_color() {
 if [[ -d "$1/.git" ]]
 then
   branch_name=$(cd $1; git rev-parse --abbrev-ref HEAD)
-  echo -n "$(branch_color $1) $branch_name "
+  echo -n "$(git_status_color $1) $branch_name "
 else 
   echo ""
 fi
